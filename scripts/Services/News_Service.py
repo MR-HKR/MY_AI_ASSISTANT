6f463623d817e4474cabce2e443f59fc92f36098
mr-kingrain <rainangelolmendoza@gmail.com>
2026-05-17 23:55:05 +0800

    6

--- scripts/Services/News_Service.py
+++ scripts/Services/News_Service.py
@@ -1,26 +1,41 @@
 import feedparser
 
-
 from html import unescape
 
 import re
+
+import time
+
 
 from scripts.python.interface.voices.TextSpeaker_Fancy import fancy_speak
 
 from storage.config.sources import SOURCES
 
+
 def clean_html(text):
     text = re.sub(r'<[^>]+>', '', text)
     return unescape(text).strip()
 
+def format_date(entry):
+    date_struct = getattr(entry, 'published_parsed', None)
+
+    if date_struct:
+        return time.strftime('%b %d, %Y - %I:%M %p', date_struct)
+
+    return (
+        getattr(entry, 'published', None)
+        or getattr(entry, 'updated', None)
+        or 'No date'
+    )
+
 # Show available sources
 sources = SOURCES.NEWS_SOURCE
-print("Available sources:")
+print('Available sources:')
 for i, station in enumerate(sources.keys(), start=1):
     print(f"{i}. {station}")
 
-# Pick
-fancy_speak('what news you wanna hear???',async_play=True)
+# Pick source
+fancy_speak('what news you wanna hear???', async_play=True)
 choice = input('\nChoose source (number or name): ').strip().lower()
 
 # Resolve choice
@@ -32,24 +47,45 @@ if choice.isdigit():
     else:
         print('Invalid number.')
         exit()
+
 elif choice in sources:
     selected = {choice: sources[choice]}
+
+elif choice.title() in sources:
+    selected = {choice.title(): sources[choice.title()]}
+
 else:
     print('Invalid choice.')
     exit()
 
+# How many articles
+while True:
+    try:
+        fancy_speak('how many articles should i read?', async_play=True)
+        choice2 = int(input('> ').strip())
+        break
+    except ValueError:
+        fancy_speak('wrong input', async_play=True)
 
-# Run
+# Run feed
 for station, source in selected.items():
     feed = feedparser.parse(source)
     print(f'----{station}----')
-    for i, entry in enumerate(feed.entries[:3], start=1):
-        print(f'{i}. {entry.title}')
-        print(clean_html(entry.summary))
+
+    for i, entry in enumerate(feed.entries[:choice2], start=1):
+        title = getattr(entry, 'title', 'No title')
+        summary = clean_html(getattr(entry, 'summary', ''))
+
+        date = format_date(entry)
+
+        print(f'{i}. {title}')
+        print(summary)
+        print(f'Date: {date}')
         print('')
-        fancy_speak(f'{i}......{entry.title}...{clean_html(entry.summary)}')
+
+        fancy_speak(f'{i}......{title}...{summary}')
 
 for i in selected.keys():
     fancy_speak(f'thats the latest news from {i}... hope youre informed')
 
-
+    
